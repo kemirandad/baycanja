f47043cb4f8e7d3699a0b5b1dee02ad13a9dcb48
Profe Kenny <facturasprofe2020@gmail.com>
2025-08-22 21:58:38 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -146,8 +146,8 @@ export default function Leaderboard() {
           {categoryParticipants
             .sort((a, b) => a.rank - b.rank)
             .map((p) => (
-              <Collapsible asChild key={p.id}>
-                <>
+              <Collapsible key={p.id} asChild>
+                <tbody>
                   <TableRow
                     className={p.rank <= 3 ? 'font-bold bg-secondary/50' : ''}
                   >
@@ -190,7 +190,7 @@ export default function Leaderboard() {
                       </TableCell>
                     </TableRow>
                   </CollapsibleContent>
-                </>
+                </tbody>
               </Collapsible>
             ))}
         </TableBody>
