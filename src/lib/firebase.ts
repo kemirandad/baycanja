3053c47c48f0523e879427c24e893e891d798f7e
Profe Kenny <facturasprofe2020@gmail.com>
2025-09-04 23:26:52 +0000

    Este el nombre del archivo: juan-jose-parejo-jimenez.png

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,7 +4,6 @@ import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
   projectId: "baycanja-ranks",
   appId: "1:579070689958:web:4ad9d2e5c417fdfbb26a67",
-  storageBucket: "baycanja-ranks.firebasestorage.app",
   apiKey: "AIzaSyAYiehaz9zL6dIVl2ZFSoV0QUGSzyflxyg",
   authDomain: "baycanja-ranks.firebaseapp.com",
   messagingSenderId: "579070689958",
